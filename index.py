8bacb01f7b5a1450b67bc0a8eb90bd7e24525e60
Mathieu Rousseau <mathieu@iMac.lan>
2020-05-08 16:47:34 +0200

    bisection method

--- index.py
+++ index.py
@@ -14,6 +14,7 @@ import numpy as np
 
 from scipy.integrate import odeint
 from scipy.optimize import newton
+from scipy.optimize import bisect
 
 from decimal import Decimal
 from edo_solver.edo import basic_blasius_edo, blasius_edo
@@ -51,32 +52,98 @@ NOTE :  we have two boundary values for the 2 coupled ODE.
 """
 def shooting(eta_range):
   # boundary values
-  fprimeinf = 0 # f'(eta -> infty) = 0
-  thetainf = 0 # theta(eta -> infty) = 0
+  #fprimeinf = 0 # f'(eta -> infty) = 0
+  #thetainf = 0 # theta(eta -> infty) = 0
 
   # initial guess
   # as far as I understand
   # it has to be the good guess
   # otherwise the result can be completely wrong
-  f_initial_guess = 10 # guess for f''(0)
-  theta_initial_guess = 100 # guess for theta'(0)
+  #f_initial_guess = 10 # guess for f''(0)
+  #theta_initial_guess = 100 # guess for theta'(0)
 
   # define our function to optimize
   # our goal is to take big eta because eta should approach infty
   # [-1, 1] : last row, second column => f'(eta_final) ~ f'(eta -> infty)
   # [-1, 3] : last row, fourth column => theta(eta_final) ~ theta(eta -> infty)
-  fun_f = lambda initial_guess: rk4(eta_range, initial_guess, theta_initial_guess)[-1, 1] - fprimeinf
-  fun_theta = lambda initial_guess: rk4(eta_range, f_initial_guess, initial_guess)[-1, 3] - thetainf
+  #fun_f = lambda initial_guess: rk4(eta_range, f_initial_guess, theta_initial_guess)[-1, 1] - fprimeinf
+  #fun_theta = lambda initial_guess: rk4(eta_range, f_initial_guess, initial_guess)[-1, 3] - thetainf
   # newton method resolve the ODE system until eta_final
   # then adjust the shoot and resolve again until we have a correct shoot
-  shoot_f = newton(func=fun_f, x0=f_initial_guess)
-  shoot_theta = newton(func=fun_theta, x0=theta_initial_guess)
+  #shoot_f = newton(func=fun_f, x0=f_initial_guess)
+  #shoot_theta = newton(func=fun_theta, x0=theta_initial_guess)
+  #shoot_f = bisect(f=fun_f, a=0, b=10)
+  #shoot_theta = bisect(f=fun_theta, a=0, b=10)
 
-  print(shoot_f, shoot_theta)
+  #print(shoot_f, shoot_theta)
+
+  shoot_flow, shoot_heat = bisection(eta_range)
+
+  print(shoot_flow, shoot_heat)
 
   # resolve our system of ODE with the good "a"
-  y = rk4(eta_range, shoot_f, shoot_theta)
+  y = rk4(eta_range, shoot_flow, shoot_heat)
   return y
+
+def bisection(eta_range):
+  # we choose a starting interval [a0, b0]
+  # such that f(a0)*f(b0) < 0
+  a0_flow = 0
+  b0_flow = 1
+
+  a0_heat = 0
+  b0_heat = 1
+
+  f_a0 = rk4(eta_range, a0_flow, a0_heat)
+  f_b0 = rk4(eta_range, b0_flow, b0_heat)
+
+  f_a0_flow = f_a0[-1, 1]
+  f_b0_flow = f_b0[-1, 1]
+
+  f_a0_heat = f_a0[-1, 3]
+  f_b0_heat = f_b0[-1, 3]
+
+  # Intermediate value problem
+  # function has a root if it changes of sign over the interval
+  if (f_a0_flow * f_b0_flow) >= 0:
+    print("Wrong interval for flow")
+    return
+
+  if (f_a0_heat * f_b0_heat) >= 0:
+    print("Wrong interval for heat")
+    return
+
+  # we decide to loop 40 times
+  for i in range(1,40):
+    # compute middle point
+    midpoint_flow = (a0_flow + b0_flow) / 2
+    midpoint_heat = (a0_heat + b0_heat) / 2
+
+    f_midpoint = rk4(eta_range, midpoint_flow, midpoint_heat)
+
+    f_midpoint_flow = f_midpoint[-1, 1]
+    f_midpoint_heat = f_midpoint[-1, 3]
+
+    # determine the next subinterval
+    if (f_a0_flow * f_midpoint_flow) < 0:
+      b0_flow = midpoint_flow
+
+    elif (f_b0_flow * f_midpoint_flow) < 0:
+      a0_flow = midpoint_flow
+
+    if (f_a0_heat * f_midpoint_heat) < 0:
+      b0_heat = midpoint_heat
+
+    elif (f_b0_heat * f_midpoint_heat) < 0:
+      a0_heat = midpoint_heat
+
+    # check precision
+    if abs(midpoint_flow) <= PRECISION and abs(midpoint_heat) <= PRECISION:
+      print("got precision")
+      break
+
+  # should be a good approximation of the root
+  return midpoint_flow, midpoint_heat
 
 def compute_blasius_edo(title, eta_final):
   ETA_0 = 0
